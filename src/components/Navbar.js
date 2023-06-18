b24f59bc6aa30da678d502674b3bb04b0a85cfbe
Bassam <basmaym148@gmail.com>
2023-06-18 18:30:13 -0400

    rename

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,6 +12,7 @@ export default function Navbar({ userId, renderDMs, setRenderDMs }){
 
     const [user, setUser] = useState(null)
     const [musicImgSrc, setMusicImgSrc] = useState(null)
+    const [renderProfileDropDown, setRenderProfileDropDown] = useState(false)
     const searchText = useRef("")
     const music = useRef(null)
     
@@ -55,7 +56,20 @@ export default function Navbar({ userId, renderDMs, setRenderDMs }){
                 <img src={DM} alt="direct messages" style={{height: "80%", cursor: "pointer"}} onClick={() => setRenderDMs(!renderDMs)}/>
                 {musicImgSrc && <img src={musicImgSrc} alt="play theme" style={{height: "50%", marginLeft: "5%", cursor: "pointer"}} onClick={playMusic}/>}
             </div>
-            <img src={noImage} alt="user" style={{cursor: "pointer", height: "65%", borderRadius: "50%", border: "0.25rem solid #cca43b"}} onClick={() => {if (user) window.location.assign(`/app/profile/${user.username}`)}} />
+            <div className="flex-100" style={{height: "100%", position: "relative", width: "5%"}}>
+                <img src={noImage} alt="user" style={{cursor: "pointer", height: "65%", borderRadius: "50%", border: "0.25rem solid #cca43b"}} onClick={() => setRenderProfileDropDown(!renderProfileDropDown)}/>
+                {renderProfileDropDown && <div style={{position: "absolute", backgroundColor: "#242f40", width: "200%", height: "150%", zIndex: 15, top: "100%", borderBottomLeftRadius: "1rem", borderBottomRightRadius: "1rem", borderLeft: "0.15rem solid #26262e", borderBottom: "0.15rem solid #26262e", borderRight: "0.15rem solid #26262e", overflow: "hidden"}}>
+                    <div className="flex-100 dropdown-half-div" onClick={() => {if (user) window.location.assign(`/app/profile/${user.username}`)}}>
+                        My Profile
+                    </div>
+                    <div className="flex-100 dropdown-half-div" onClick={() => {
+                        document.cookie.split(";").forEach(function(c) { document.cookie = c.replace(/^ +/, "").replace(/=.*/, "=;expires=" + new Date().toUTCString() + ";path=/"); });
+                        window.location.assign("/signup")
+                    }}>
+                        Log Out
+                    </div>
+                </div>}
+            </div>
         </nav>
     )
 }
